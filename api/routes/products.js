1988aec75532eb25be11bbd827b2776ee1ce67eb
Cobee <uniqtop91@gmail.com>
2019-03-08 11:27:22 +0900

    JWT authentication to access Route

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+const checkAuth = require('../middleware/check-auth');
 const Product = require('../models/product');
 
 
@@ -34,7 +35,7 @@ router.get('/', (req, res, next) => {
         })
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     const product = new Product({
         name: req.body.name,
         price: req.body.price
@@ -94,7 +95,7 @@ router.get('/:productId', (req, res, next) => {
         });
 })
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -120,7 +121,7 @@ router.patch('/:productId', (req, res, next) => {
         })
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.remove({_id: id})
         .exec()
